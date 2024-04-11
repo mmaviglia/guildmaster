1e2a10bf0adbb19a8c2845e9371dc4e2961ba6df
mmaviglia <mmaviglia@ec2software.com>
2024-04-10 20:42:14 -0700

    implement experience points on guild activity

--- internal/models/guild_activity.go
+++ internal/models/guild_activity.go
@@ -14,6 +14,7 @@ type GuildActivity struct {
 	UserID              string    `gorm:"index"`
 	MessagesSent        int       `gorm:"type:bigint"`
 	VoiceChannelSeconds int       `gorm:"type:bigint"`
+	Experience          int       `gorm:"index;type:bigint"`
 }
 
 // Return the guild activity using the user and guild ID strings.
@@ -26,6 +27,14 @@ func GetGuildActivity(userID, guildID string) (*GuildActivity, error) {
 func GetGuildActivityByID(id uuid.UUID) (*GuildActivity, error) {
 	m := &GuildActivity{}
 	return m, DB.First(m, "id = ?", id).Error
+}
+
+// Return the activity records for a particular server, ranked by XP.
+func GetServerLeaderboard(guildID string) ([]GuildActivity, error) {
+	activities := []GuildActivity{}
+	tx := DB.Model(&GuildActivity{}).Order("experience DESC").Where("guild_id = ?", guildID)
+
+	return activities, tx.Find(&activities).Error
 }
 
 // Create a GuildActivity record in the database.
@@ -42,7 +51,10 @@ func guildActivityID(userID, guildID string) uuid.UUID {
 // Increment the number of messages sent by the user within the given guild.
 func IncrementMessagesSent(userID, guildID string) error {
 	tx := DB.Model(&GuildActivity{}).Where("id = ?", guildActivityID(userID, guildID))
-	result := tx.UpdateColumn("messages_sent", gorm.Expr("messages_sent + 1"))
+	result := tx.Updates(map[string]interface{}{
+		"messages_sent": gorm.Expr("messages_sent + 1"),
+		"experience":    gorm.Expr("experience + 1"),
+	})
 
 	if result.Error != nil {
 		return result.Error
@@ -53,6 +65,7 @@ func IncrementMessagesSent(userID, guildID string) error {
 			UserID:       userID,
 			GuildID:      guildID,
 			MessagesSent: 1,
+			Experience:   1,
 		})
 		if err != nil {
 			return fmt.Errorf("create guild activity: %w", err)
@@ -65,7 +78,10 @@ func IncrementMessagesSent(userID, guildID string) error {
 // Increment the duration spent within voice channels by the user within the given guild.
 func IncrementVoiceChannelDuration(userID, guildID string, duration int) error {
 	tx := DB.Model(&GuildActivity{}).Where("id = ?", guildActivityID(userID, guildID))
-	result := tx.UpdateColumn("voice_channel_seconds", gorm.Expr("voice_channel_seconds + ?", duration))
+	result := tx.Updates(map[string]interface{}{
+		"voice_channel_seconds": gorm.Expr("voice_channel_seconds + ?", duration),
+		"experience":            gorm.Expr("experience + ?", duration/60),
+	})
 
 	if result.Error != nil {
 		return result.Error
@@ -76,6 +92,7 @@ func IncrementVoiceChannelDuration(userID, guildID string, duration int) error {
 			UserID:              userID,
 			GuildID:             guildID,
 			VoiceChannelSeconds: duration,
+			Experience:          duration / 60,
 		})
 		if err != nil {
 			return fmt.Errorf("create guild activity: %w", err)
